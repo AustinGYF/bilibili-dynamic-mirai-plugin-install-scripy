32b0deaa0e3e4c2b33bcf283f82a2a40e73d592f
AustinGYF <87134663+AustinGYF@users.noreply.github.com>
2022-11-14 01:28:09 +0000

    Add files via upload

---
+++ to-add.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./mcl --update-package xyz.cssxsh.mirai:mirai-skia-plugin --channel maven-stable --type plugin
+./mcl --update-package net.mamoe:chat-command --channel stable --type plugin
+./mcl --update-package top.colter:bilibili-dynamic-mirai-plugin --channel maven --type plugin
